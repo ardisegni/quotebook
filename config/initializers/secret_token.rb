4b86dc803f9ae74810a335a15bcf0a12a1148522
Ariel Di Segni <ardisegni@gmail.com>
2012-12-12 19:15:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quotebook::Application.config.secret_token = '1b5afc2918d2b3b1ce933ddcebe2c0d6722822b35693a262c332afeed696d8b635d867a8599262e72c5224c8fb3c463150f903357438a744ac4b015303c1bc42'
